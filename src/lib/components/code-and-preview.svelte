7c1e2a414c3be2868d79e7d575e955e0823d8513
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-01-17 18:02:58 +0100

    code components

--- src/lib/components/code-and-preview.svelte
+++ src/lib/components/code-and-preview.svelte
@@ -17,13 +17,13 @@
     </Tabs.List>
     <Tabs.Content value="preview">
         <div
-            class="flex min-h-100 w-full flex-row items-center justify-center rounded-md border-2 p-2 py-10"
+            class="flex min-h-100 w-full flex-row items-center justify-center rounded-md border p-2 py-10 bg-card"
         >
             {@render children?.()}
         </div>
     </Tabs.Content>
     <Tabs.Content value="code">
-        <div class="w-full">
+        <div class="w-full -2">
             <Code.Root {lang} class="h-min max-h-100" code={code.trim()}>
                 <Code.CopyButton />
             </Code.Root>
--- src/lib/components/code-block.svelte
+++ src/lib/components/code-block.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import * as Code from '$lib/components/ui/code';
     import type { SupportedLanguage } from './ui/code/shiki';
-    let { code, lang }: { code: string; lang: SupportedLanguage } = $props();
+    let { code, lang = 'svelte' }: { code: string; lang?: SupportedLanguage } =
+        $props();
 </script>
 
-<div class="w-full">
+<div class="w-full py-2">
     <Code.Root {lang} class="h-min" {code}>
         <Code.CopyButton />
     </Code.Root>

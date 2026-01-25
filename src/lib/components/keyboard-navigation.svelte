3ee8920a5b7885758e588b512fd0b2d0906ca7fa
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-01-25 12:34:46 +0100

    keyboard navigation component

---
+++ src/lib/components/keyboard-navigation.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import * as Kbd from '$lib/components/ui/kbd/index.js';
+    import * as Table from '$lib/components/ui/table/index.js';
+    let { interactions }: { interactions: { keys: string[]; desc: string }[] } =
+        $props();
+</script>
+
+<div class="m-1 rounded-md border">
+    <Table.Root>
+        <Table.Header class='bg-card'>
+            <Table.Row>
+                <Table.Head class="w-25">Key</Table.Head>
+                <Table.Head>Description</Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {#each interactions as int}
+                <Table.Row>
+                    <Table.Cell class="font-medium"
+                        >{@render keysRenderer(int.keys)}</Table.Cell
+                    >
+                    <Table.Cell>{int.desc}</Table.Cell>
+                </Table.Row>
+            {/each}
+        </Table.Body>
+    </Table.Root>
+</div>
+
+{#snippet keysRenderer(keys: string[])}
+    {#if keys.length === 1}
+        <Kbd.Root>{keys[0]}</Kbd.Root>
+    {:else}
+        <Kbd.Group>
+            {#each keys as k}
+                <Kbd.Root>{k}</Kbd.Root>
+            {/each}
+        </Kbd.Group>
+    {/if}
+{/snippet}

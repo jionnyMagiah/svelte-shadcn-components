e28515de0966dfcbf0e0919963b4ae90f523b690
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-01-27 08:32:24 +0100

    login block example

---
+++ src/routes/(docs)/blocks/originals/login/login-01/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Login01 from "routes/(docs)/blocks/originals/login/login-01/login-01.svelte";
+
+</script>
+<Login01 />
---
+++ src/routes/(docs)/blocks/originals/login/login-01/login-01.svelte
@@ -0,0 +1,5 @@
+<script>
+    import ExampleInline from "routes/(docs)/components/dice-ui/color-picker/example-inline.svelte";
+
+</script>
+<ExampleInline />

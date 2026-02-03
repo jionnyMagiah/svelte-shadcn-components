e70d8707009e898152658d6f1742cb9eefe4210a
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-02-03 20:34:32 +0100

    link snippet

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" module>
+    import { type ResolvedPathname } from '$app/types';
+    export { link };
+</script>
+
+{#snippet link(label: string, url: ResolvedPathname)}
+    <a href={url} class="link">{label}</a>
+{/snippet}

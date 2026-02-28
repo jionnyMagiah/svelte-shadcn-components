260f58e6a7d3896b825f7da2bdef30a96fd354aa
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-02-28 21:57:18 +0100

    blueprint

--- src/lib/blueprints/default/blueprint.svelte
+++ src/lib/blueprints/default/blueprint.svelte
@@ -1,13 +1,14 @@
 <script lang="ts" module>
     export { default as a } from './a.svelte';
+    export { default as code } from './code.svelte';
     export { default as h1 } from './h1.svelte';
     export { default as h2 } from './h2.svelte';
     export { default as h3 } from './h3.svelte';
     export { default as h4 } from './h4.svelte';
     export { default as li } from './li.svelte';
-    export { default as ul } from './ul.svelte';
+    export { default as p } from './p.svelte';
     export { default as pre } from './pre.svelte';
-    export { default as code } from './code.svelte';
+    export { default as ul } from './ul.svelte';
 </script>
 
 <script lang="ts">

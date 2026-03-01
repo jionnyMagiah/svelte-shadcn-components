e61b54e4604ba2a756e153fa5067433fd917e129
Jionny Magiah <giovanni.dibari15@gmail.com>
2026-03-01 10:07:47 +0100

    props

--- src/lib/props/display-prop.svelte
+++ src/lib/props/display-prop.svelte
@@ -1,27 +1,42 @@
-<script lang="ts" module>
-    export type PropDesc = {
-        name: string;
-        desc?: string;
-        type: string;
-        default?: string;
-        bindable?: boolean;
-    };
-</script>
-
 <script lang="ts">
     import * as Accordion from '$lib/components/ui/accordion/index.js';
-    import Badge from './ui/badge/badge.svelte';
-    let { props: propsArray }: { props: PropDesc[] } = $props();
+    import rehypeStringify from 'rehype-stringify';
+    import remarkParse from 'remark-parse';
+    import remarkRehype from 'remark-rehype';
+    import { getProps } from 'routes/api/api.remote';
+    import { unified } from 'unified';
+    import Badge from '../components/ui/badge/badge.svelte';
+
+    type PropsData = Awaited<ReturnType<typeof getProps>>;
+
+    let {
+        name
+    }: {
+        name: keyof PropsData;
+    } = $props();
+
+    const data = $derived(await getProps());
+
+    const componentProps = $derived(data[name]);
+
+    async function getMDDesc(desc: string) {
+        const parsed = await unified()
+            .use(remarkParse)
+            .use(remarkRehype, { allowDangerousHtml: true })
+            .use(rehypeStringify)
+            .process(desc);
+        return String(parsed);
+    }
 </script>
 
-{#if propsArray.length === 0}
+{#if !componentProps || componentProps.length === 0}
     <div class="my-2 rounded-md border p-4 text-muted-foreground">
         No props to display
     </div>
 {:else}
     <div class="my-2 rounded-md border p-2">
         <Accordion.Root type="multiple">
-            {#each propsArray as prop (prop.name)}
+            {#each componentProps as prop (prop.name)}
                 <Accordion.Item>
                     <Accordion.Trigger class="hover:no-underline">
                         <div
@@ -31,7 +46,7 @@
                                 <span class="font-mono font-medium"
                                     >{prop.name}</span
                                 >
-                                {#if prop.bindable}
+                                {#if 'bindable' in prop && prop.bindable}
                                     <Badge class="rounded-sm">Bindable</Badge>
                                 {/if}
                             </div>
@@ -45,7 +60,9 @@
                     <Accordion.Content
                         class="mb-2 flex flex-col gap-2 rounded-md bg-card p-2 text-[1rem]"
                     >
-                        <p class="mb-4">{prop.desc}</p>
+                        <p class="mb-4">
+                            {@html await getMDDesc(prop.desc ?? '')}
+                        </p>
 
                         <div
                             class="grid w-full grid-cols-[30%_1fr] items-center"
--- src/routes/api/api.remote.ts
+++ src/routes/api/api.remote.ts
@@ -1,6 +1,7 @@
 import { query } from '$app/server';
 import data from './code-preview.json' assert { type: 'json' };
 import search from './search.json' assert { type: 'json' };
+import props from './props.json' assert { type: 'json' };
 
 export const getSearch = query(async () => {
     return search.filter((s) => s.group !== 'index');
@@ -8,4 +9,8 @@ export const getSearch = query(async () => {
 
 export const getCodepreview = query(async () => {
     return data;
+});
+
+export const getProps = query(async () => {
+    return props;
 });
